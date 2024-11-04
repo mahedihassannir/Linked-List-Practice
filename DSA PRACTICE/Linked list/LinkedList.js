c590d1efb260243443159ea7a533c20bd059374a
mahedihasssannir <mahedinir34678@gmail.com>
2024-11-05 02:12:29 +0600

    linked list practic

--- DSA PRACTICE/Linked list/LinkedList.js
+++ DSA PRACTICE/Linked list/LinkedList.js
@@ -1,3 +1,8 @@
+// Do triverse operation
+// Do dletion
+// insertion
+
+
 class linkedList {
     constructor(data) {
 
@@ -19,10 +24,47 @@ class linkedList {
         this.size++
     };
 
+    deleteNode = (index) => {
+        let counter = 1;
+        let lead = this.head;
+        if (index === 1) {
+            lead = this.head.next;
+        };
+        while (counter < index - 1) {
+            lead = lead.next;
+            counter++
+        };
+        let nextNode = lead.next.next;
+        lead.next = nextNode;
+    };
+    insertNode = (index, value) => {
+        let currentIndex = 1;
+        let currentNode = this.head;
+        if (index === 1) {
+            this.head = {
+                value: value,
+                next: currentNode.next
+            }
+        }
+        while (currentIndex < index) {
+            currentIndex++;
+            currentNode = currentNode.next;
+        };
+        let nextNode = currentNode.next;
+        console.log(nextNode);
+        currentNode.next = {
+            value: value,
+            next: nextNode
+        };
+        this.size++
+    };
 };
 
 let linkedLists = new linkedList(10)
 linkedLists.appendNode(20)
 linkedLists.appendNode(30)
 linkedLists.appendNode(40)
+// linkedLists.deleteNode()
+linkedLists.insertNode(2, 666);
+linkedLists.insertNode(1, 111);
 console.log(linkedLists)
